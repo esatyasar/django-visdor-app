062f42bc9524b532f84724ca78eace4567c56837
esatyasar <esatyasar91@gmail.com>
2021-11-01 21:56:33 +0300

    added sttaic folder and template

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "phone_number",
+        "email",
+        "message",
+        "created_date",
+    )
+
+    list_filter = ("name",)
+    search_fields = ("name_startswith",)
+    
+    
+admin.site.register(Contact,ContactAdmin)
 
 # Register your models here.
